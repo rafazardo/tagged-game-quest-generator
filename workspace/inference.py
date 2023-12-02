bd2ab115d97cd051e67b844ed065eea992d7e410
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2023-12-02 17:34:29 -0300

    Update inference.py

--- workspace/inference.py
+++ workspace/inference.py
@@ -1,20 +1,52 @@
 from transformers import pipeline
+import argparse
+import os
 
-PATH_OUTDIR_SENTENCES = ""
-NUM_SENTENCES = 5
+def is_valid_directory(path):
+    """
+    Checks if the provided path is a valid directory.
 
-quest_generator = pipeline('text-generation', model='./gpt2-quest-generator', tokenizer='gpt2', max_length=10000, temperature=0.5)
+    Arguments:
+    path (str): Path to be checked.
 
-sentences = []
+    Outputs:
+    bool: Returns True if the path is a valid directory, False otherwise.
+    """
+    if os.path.isdir(path):
+        return True
+    return False
 
-for i in range(NUM_SENTENCES):
-    speech = quest_generator("<SOS>", max_length=100, min_length=10, do_sample=True, temperature=1.2)[0]['generated_text']
-    sentences.append(speech)
-    if '<EOS>' in speech:
-        break
+if __name__ == "__main__":
 
-with open(PATH_OUTDIR_SENTENCES, 'w', encoding='utf-8') as out_file:
-    for sentence in sentences:
-        if sentence[:-1] != '\n':
-            sentence += '\n'
-        out_file.write(sentence)
+    # Setting up the parser to accept command line arguments
+    parser = argparse.ArgumentParser(description='Download dataset to a specific directory.')
+    parser.add_argument('--path_outdir_sentences', type=str, required=True, help='Destination path to save the generated sentences')
+    parser.add_argument('--num_sentences', type=int, required=True, help='Number of sentences to be generated')
+
+    # Parsing command line arguments
+    args = parser.parse_args()
+    output_directory = args.path_outdir_sentences
+    num_sentences = args.num_sentences
+
+    # Checking if the provided path is a valid directory
+    if not is_valid_directory(output_directory):
+        print("The provided path is not a valid directory.")
+    else:
+        # Constructing the output file path
+        output_file = os.path.join(output_directory, "tvgqs.txt")
+
+    quest_generator = pipeline('text-generation', model='./gpt2-quest-generator', tokenizer='gpt2', max_length=10000, temperature=0.5)
+
+    sentences = []
+
+    for i in range(num_sentences):
+        speech = quest_generator("<SOS>", max_length=100, min_length=10, do_sample=True, temperature=1.2)[0]['generated_text']
+        sentences.append(speech)
+        if '<EOS>' in speech:
+            break
+
+    with open(output_directory, 'w', encoding='utf-8') as out_file:
+        for sentence in sentences:
+            if sentence[:-1] != '\n':
+                sentence += '\n'
+            out_file.write(sentence)
