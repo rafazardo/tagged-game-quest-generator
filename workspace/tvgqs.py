27f3014c25f2121032308aedeba931d1c3ac4dac
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2023-12-02 16:49:15 -0300

    Update tvgqs.py

--- workspace/tvgqs.py
+++ workspace/tvgqs.py
@@ -5,6 +5,7 @@ Author: Andre Feijó, Pedro Fiorio, Rafael Zardo
 Date: 11/02/2023
 """
 
+from transformers import TextDataset,DataCollatorForLanguageModeling
 from sklearn.model_selection import train_test_split
 import csv
 import re
@@ -17,9 +18,6 @@ class TVGQS:
             Args:
                 path_indir_dataset (str): Path to the dataset csv file.
                 test_size (float): Percentage of the data destined to test.
-
-            Returns:
-                tipo_de_retorno: Descrição do que o método retorna.
         """
 
         self.path_indir_dataset = path_indir_dataset
@@ -53,6 +51,34 @@ class TVGQS:
             path_outdir_test_txt (str): Path to create the test file.
         """
         self.build_text_file(self.test, path_outdir_test_txt)
+
+    def load_dataset(self, path_indir_train_txt, path_indir_test_txt, tokenizer):
+        """
+        Loads the train, test and collator data
+
+        Args:
+            path_indir_train_txt (str): .
+            path_indir_test_txt (str): Path to create the test file.
+            tokenizer (str): Path to create the test file.
+        
+        Return:
+            train_TVGQS_dataset, test_TVGQS_dataset, data_collator
+        """
+        train_TVGQS_dataset = TextDataset(
+            tokenizer=tokenizer,
+            file_path=path_indir_train_txt,
+            block_size=32)
+
+        test_TVGQS_dataset = TextDataset(
+            tokenizer=tokenizer,
+            file_path=path_indir_test_txt,
+            block_size=32)
+
+        data_collator = DataCollatorForLanguageModeling(
+            tokenizer=tokenizer, mlm=False,
+
+        )
+        return train_TVGQS_dataset, test_TVGQS_dataset, data_collator
 
     def _build_text_file(self, txts, dest_path):
         with open(dest_path, 'w') as f:
