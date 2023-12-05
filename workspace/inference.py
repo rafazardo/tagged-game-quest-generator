9bb536ff86a5c4ad6a45e4d96c57b9c4c8f02fbe
PedroFiorio <94929673+PedroFiorio@users.noreply.github.com>
2023-12-04 21:46:37 -0300

    Update inference.py

--- workspace/inference.py
+++ workspace/inference.py
@@ -15,7 +15,7 @@ def is_valid_directory(path):
     """
     return os.path.isdir(path)
 
-def run_inference(trained_model_path, output_directory, num_sentences):
+def run_inference(trained_model_path, output_directory, num_sentences, temperature):
     """
     Runs the inference using the provided model and generates sentences.
 
@@ -23,6 +23,7 @@ def run_inference(trained_model_path, output_directory, num_sentences):
     trained_model_path (str): Path to the trained model.
     output_directory (str): Destination path to save the generated sentences.
     num_sentences (int): Number of sentences to be generated.
+    temperature (float): Hiperparameter to control randomness of output 
 
     This function initializes the model, generates sentences using the provided model path,
     and saves the generated sentences to the specified output directory.
@@ -33,7 +34,7 @@ def run_inference(trained_model_path, output_directory, num_sentences):
 
     # Generate sentences until <EOS> token is encountered or the specified number of sentences is reached
     for i in range(num_sentences):
-        speech = quest_generator("<SOS>", max_length=200, min_length=10, do_sample=True, temperature=1.2)[0]['generated_text']
+        speech = quest_generator("<SOS>", max_length=200, min_length=10, do_sample=True, temperature=temperature)[0]['generated_text']
     
         # Verifica se <EOS> está presente na string
         if '<EOS>' in speech:
@@ -56,12 +57,14 @@ if __name__ == "__main__":
     parser.add_argument('--path_outdir_sentences', type=str, required=True, help='Destination path to save the generated sentences')
     parser.add_argument('--path_indir_trained_model', type=str, required=True, help='Destination get the trained model')
     parser.add_argument('--num_sentences', type=int, required=True, help='Number of sentences to be generated')
+    parser.add_argument('--temperature', type=float, default=1.2, help='Hiperparameter to control randomness of output')
 
     # Parsing command line arguments
     args = parser.parse_args()
     output_directory = args.path_outdir_sentences
     trained_model_path = args.path_indir_trained_model
     num_sentences = args.num_sentences
+    temperature = args.temperature
 
     # Checking if the provided path is a valid directory
     if not is_valid_directory(output_directory):
@@ -71,4 +74,4 @@ if __name__ == "__main__":
         output_file = os.path.join(output_directory, "tvgqs.txt")
 
         # Running the inference
-        run_inference(trained_model_path, output_directory, num_sentences)
+        run_inference(trained_model_path, output_directory, num_sentences, temperature)
